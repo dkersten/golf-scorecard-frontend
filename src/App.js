fb63dce6b93ee00b5155ba4a77872429ac1f61f1
dkersten <dkersten1994@gmail.com>
2020-06-26 14:33:45 -0400

    set up scss env. and landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Route, Switch, Redirect} from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from './components/Nav.js';
+import Landing from './components/Landing.js';
 
-function App() {
-  return (
-    <div className="App">
-      Golf
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    courses: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/courses')
+      .then(resp => resp.json())
+      .then(courses => this.setState({courses}))
+  }
+
+  render() {
+    
+    return (
+      <div className="App">
+        <Nav />
+        <Landing />
+      </div>
+    );
+  }
 }
 
 export default App;
